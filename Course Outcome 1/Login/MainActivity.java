2350a737c68391365987fc4c13b04b82bc1917c6
JOMIN K MATHEW <jominachu001@gmail.com>
2022-02-03 01:22:36 +0530

    lab

--- Course Outcome 1/Login/MainActivity.java
+++ Course Outcome 1/Login/MainActivity.java
@@ -7,31 +7,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
-    EditText e1,e2;
+public class MainActivity extends AppCompatActivity {
+    EditText t1,t2;
     Button b1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1=(EditText) findViewById(R.id.name);
-        e2=(EditText)findViewById(R.id.password);
-        b1=(Button)findViewById(R.id.login);
+        t1=(EditText)findViewById(R.id.name);
+        t2=(EditText)findViewById(R.id.password);
+        b1=(Button)findViewById((R.id.login));
     }
-
-    public void fn(View view) {
-        String n = e1.getText().toString();
-        String w = e2.getText().toString();
-        if((n.equals("admin"))&&(w.equals("admin")))
+    public void onClick(View view){
+        String a =t1.getText().toString();
+        String b =t2.getText().toString();
+        if((a.equals("admin"))&&(b.equals("admin")))
         {
-            Toast.makeText(this, "Succesfully logined", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Succesfully Logined",Toast.LENGTH_LONG).show();
         }
         else{
-            Toast.makeText(this, "Unable to login check username & password", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Check username & password",Toast.LENGTH_LONG).show();
         }
-
     }
 }
